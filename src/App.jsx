05856ffd7d9badba802381f3eeb8fd7eaedeaaa4
Anahitashams <anahitashams01@gmail.com>
2024-12-28 11:16:51 -0800

    saved page2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from '../components/header/Header'
 import Back from '../components/bg/Back'
 import Box from '../components/boxs/Box'
+import Page2 from '../components/cards/Page2'
 
 
 
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Back/>
       <Box/>
+      <Page2/>
     </>
   )
 }
