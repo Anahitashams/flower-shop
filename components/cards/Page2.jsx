8f04ac87e12fea3c1576136b5bbb3ea20b5f8bda
Anahitashams <anahitashams01@gmail.com>
2024-12-28 11:14:45 -0800

    saved page2

---
+++ components/cards/Page2.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Page2() {
+  return (
+    <>
+    <div className="parent flex flex-wrap justify-center">
+        <div><img src="../public/img/buy-plants.jpg.webp" alt="" /></div>
+        <div><img src="../public/img/Flower-Basket-VIP-250x250.jpg.webp" alt="" /></div>
+        <div><img src="../public/img/Flower-Bouquet-VIP.jpg.webp" alt="" /></div>
+        <div><img src="../public/img/flower-stand.webp" alt="" /></div>
+        <div><img src="../public/img/IMG_207777777777777777777777777-1.jpg.webp" alt="" /></div>
+        <div><img src="../public/img/luxury-flower-box-2-min-min (1).jpg" alt="" /></div>
+        <div><img src="../public/img/Sympathy-Flowers-VIP.jpg.webp" alt="" /></div>
+        <div><img src="../public/img/Terrarium-VIP.jpg.webp" alt="" /></div>
+
+    </div>
+    
+    </>
+  )
+}
+
+export default Page2
