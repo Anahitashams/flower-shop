739eb6fd495c56352fc521567892805482ecd2b0
Anahitashams <anahitashams01@gmail.com>
2024-12-29 00:01:18 -0800

    end

--- components/cards/Page2.jsx
+++ components/cards/Page2.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
 
 function Page2() {
-  return (
-    <>
-    <div className="parent flex flex-wrap justify-center">
-        <div><img src="../public/img/buy-plants.jpg.webp" alt="" /></div>
-        <div><img src="../public/img/Flower-Basket-VIP-250x250.jpg.webp" alt="" /></div>
-        <div><img src="../public/img/Flower-Bouquet-VIP.jpg.webp" alt="" /></div>
-        <div><img src="../public/img/flower-stand.webp" alt="" /></div>
-        <div><img src="../public/img/IMG_207777777777777777777777777-1.jpg.webp" alt="" /></div>
-        <div><img src="../public/img/luxury-flower-box-2-min-min (1).jpg" alt="" /></div>
-        <div><img src="../public/img/Sympathy-Flowers-VIP.jpg.webp" alt="" /></div>
-        <div><img src="../public/img/Terrarium-VIP.jpg.webp" alt="" /></div>
+    return (
+        <>
+            <div className="parent flex-col flex-wrap justify-center">
+                <div className="first flex justify-center">
+                    <div className='w-[300px] h-[300px] m-[3px]  hover:rotate-2'><img src="../public/img/buy-plants.jpg.webp" alt="" /></div>
+                    <div className='w-[300px] h-[300px] m-[3px]  hover:rotate-2 '><img src="../public/img/Sympathy-Flowers-VIP.jpg.webp" alt="" /></div>
+                    <div className='w-[300px] h-[300px] m-[3px]  hover:rotate-2'><img src="../public/img/Flower-Bouquet-VIP.jpg.webp" alt="" /></div>
+                    <div className='w-[300px] h-[300px] m-[3px]  hover:rotate-2'><img src="../public/img/flower-stand.webp" alt="" /></div>
+                </div>
+                <div className="secound flex justify-center">
+                    <div className='w-[300px] h-[300px] m-[3px]  hover:rotate-2'><img src="../public/img/IMG_207777777777777777777777777-1.jpg.webp" alt="" /></div>
+                    <div className='w-[300px] h-[300px] m-[3px]  hover:rotate-2'><img src="../public/img/luxury-flower-box-2-min-min (1).jpg" alt="" /></div>
+                    <div className='w-[300px] h-[300px] m-[3px]  hover:rotate-2'><img src="../public/img/Sympathy-Flowers-VIP.jpg.webp" alt="" /></div>
+                    <div className='w-[300px] h-[300px] m-[3px]  hover:rotate-2'><img src="../public/img/Terrarium-VIP.jpg.webp" alt="" /></div>
+                </div>
 
-    </div>
-    
-    </>
-  )
+            </div>
+
+        </>
+    )
 }
 
 export default Page2
