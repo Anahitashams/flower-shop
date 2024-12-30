4bf52f1a3b34de35e610d8e5c0a896d65a30b0be
Anahitashams <anahitashams01@gmail.com>
2024-12-30 05:01:22 -0800

    end

--- components/cards/Page2.jsx
+++ components/cards/Page2.jsx
@@ -5,7 +5,22 @@ function Page2() {
         <>
             <div className="parent flex-col flex-wrap justify-center">
                 <div className='flex justify-center'>
-                    <div className='w-[300px] h-[300px] m-[3px] relative overflow-hidden'><img className='w-full h-full hover:scale-110 transition-transform duration-1000 ' src="../public/img/buy-plants.jpg.webp" alt="" /><div className='flex justify-start z-50 mt-[-60px] size-4 bg-gray-700/40'><h2>hiii</h2></div></div>
+                    <div className='w-[300px] h-[300px] m-[3px] relative overflow-hidden'><img className='w-full h-full hover:scale-110 transition-transform duration-1000' src="../public/img/buy-plants.jpg.webp" alt="" />
+                    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                     <div className='w-[300px] h-[300px] m-[3px]  hover:rotate-2 '><img src="../public/img/Sympathy-Flowers-VIP.jpg.webp" alt="" /></div>
                     <div className='w-[300px] h-[300px] m-[3px]  hover:rotate-2'><img src="../public/img/Flower-Bouquet-VIP.jpg.webp" alt="" /></div>
                     <div className='w-[300px] h-[300px] m-[3px]  hover:rotate-2'><img src="../public/img/flower-stand.webp" alt="" /></div>
