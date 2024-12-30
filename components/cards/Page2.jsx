bf70f20fdc8e44e462d61f04d0c7e479d4518d7c
Anahitashams <anahitashams01@gmail.com>
2024-12-30 05:06:45 -0800

    end

--- components/cards/Page2.jsx
+++ components/cards/Page2.jsx
@@ -6,16 +6,16 @@ function Page2() {
             <div className="parent flex-col flex-wrap justify-center">
                 <div className='flex justify-center'>
                     <div className='w-[300px] h-[300px] m-[3px] relative overflow-hidden'><img className='w-full h-full hover:scale-110 transition-transform duration-1000' src="../public/img/buy-plants.jpg.webp" alt="" /></div>
-                    <div className='w-[300px] h-[300px] m-[3px] relative overflow-hidden'><img src="../public/img/Sympathy-Flowers-VIP.jpg.webp" alt="" /></div>
-                    <div className='w-[300px] h-[300px] m-[3px] relative overflow-hidden'><img src="../public/img/Flower-Bouquet-VIP.jpg.webp" alt="" /></div>
-                    <div className='w-[300px] h-[300px] m-[3px] relative overflow-hidden'><img src="../public/img/flower-stand.webp" alt="" /></div>
+                    <div className='w-[300px] h-[300px] m-[3px] relative overflow-hidden'><img className='w-full h-full hover:scale-110 transition-transform duration-1000' src="../public/img/Sympathy-Flowers-VIP.jpg.webp" alt="" /></div>
+                    <div className='w-[300px] h-[300px] m-[3px] relative overflow-hidden'><img className='w-full h-full hover:scale-110 transition-transform duration-1000' src="../public/img/Flower-Bouquet-VIP.jpg.webp" alt="" /></div>
+                    <div className='w-[300px] h-[300px] m-[3px] relative overflow-hidden'><img className='w-full h-full hover:scale-110 transition-transform duration-1000' src="../public/img/flower-stand.webp" alt="" /></div>
                 </div>
 
                 <div className="secound flex justify-center">
-                    <div className='w-[300px] h-[300px] m-[3px]  hover:rotate-2'><img src="../public/img/IMG_207777777777777777777777777-1.jpg.webp" alt="" /></div>
-                    <div className='w-[300px] h-[300px] m-[3px]  hover:rotate-2'><img src="../public/img/luxury-flower-box-2-min-min (1).jpg" alt="" /></div>
-                    <div className='w-[300px] h-[300px] m-[3px]  hover:rotate-2'><img src="../public/img/Sympathy-Flowers-VIP.jpg.webp" alt="" /></div>
-                    <div className='w-[300px] h-[300px] m-[3px]  hover:rotate-2'><img src="../public/img/Terrarium-VIP.jpg.webp" alt="" /></div>
+                    <div className='w-[300px] h-[300px] m-[3px] relative overflow-hidden'><img src="../public/img/IMG_207777777777777777777777777-1.jpg.webp" alt="" /></div>
+                    <div className='w-[300px] h-[300px] m-[3px] relative overflow-hidden'><img src="../public/img/luxury-flower-box-2-min-min (1).jpg" alt="" /></div>
+                    <div className='w-[300px] h-[300px] m-[3px] relative overflow-hidden'><img src="../public/img/Sympathy-Flowers-VIP.jpg.webp" alt="" /></div>
+                    <div className='w-[300px] h-[300px] m-[3px] relative overflow-hidden'><img src="../public/img/Terrarium-VIP.jpg.webp" alt="" /></div>
                 </div>
 
             </div >
