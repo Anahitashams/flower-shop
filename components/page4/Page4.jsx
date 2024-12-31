200b88c4b5d2560f832c51063cfc85205b37b958
Anahitashams <anahitashams01@gmail.com>
2024-12-31 03:57:26 -0800

    end

--- components/page4/Page4.jsx
+++ components/page4/Page4.jsx
@@ -6,7 +6,7 @@ function Page4() {
             <div className='w-full h-[500px] flex flex-col'>
                 <div className='group size-96 bg-blue-100 dark:bg-gray-900 flex flex-col'>
                     <p className='text-center flex justify-center group-hover:text-[#fff]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, magnam adipisci. Iure possimus eum, asperiores explicabo veritatis, deleniti dicta labore consequatur molestias quibusdam quaerat, fugit magnam repellendus! Ab, tempore soluta!</p>
-                    <button className='size-16 bg-blue-400 text-black group-hover:bg-blue-950 m-auto group-hover:text-[#fff] mt-[100px]'>click</button>
+                    <button className='size-16 bg-blue-400 text-black group-hover:bg-blue-950 flex justify-center group-hover:text-[#fff] mt-[100px]'>click</button>
                 </div>
             </div>
         </>
