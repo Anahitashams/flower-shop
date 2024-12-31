17c4f54915f065dec679aa3f5efd45a605c48293
Anahitashams <anahitashams01@gmail.com>
2024-12-31 01:42:19 -0800

    end

--- components/page3/Page3.jsx
+++ components/page3/Page3.jsx
@@ -3,26 +3,22 @@ import React from 'react'
 function Page3() {
     return (
         <>
-        <div className='w-100% h-[500px] bg-gray-100 '>
-        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <a href="#">
-                    <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
-                </a>
-                <div class="p-5">
+            <div className="w-[100%] h-[900px] bg-gray-100 mt-[40px] flex flex-col">
+                <h1 className=' decoration-emerald-950 flex justify-center mt-16 text-2xl'>فروش محصولات ویژه</h1>
+                <div class="m-auto mt-[60px] max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                     <a href="#">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                        <img class="rounded-t-lg" src="../public/img/IMG_207777777777777777777777777-1.jpg.webp" alt="" />
                     </a>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        Read more
-                        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                        </svg>
-                    </a>
-                </div>
-            </div>
-        </div>
+                    <div class="p-5">
+                        <a href="#">
+                            <h5 class="mb-2 text-lg tracking-tight text-gray-900 dark:text-white text-center">گل های زیبا برای همه مناسبت ها</h5>
+                        </a>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 text-left">2.500 تومان</p>
+                    </div>
 
+                </div>
+
+            </div>
         </>
     )
 }
