5b04b95737c13584d4dde8e6f87ee780a87e768a
Anahitashams <anahitashams01@gmail.com>
2024-12-31 03:07:42 -0800

    end

--- components/page3/Page3.jsx
+++ components/page3/Page3.jsx
@@ -3,17 +3,18 @@ import React from 'react'
 function Page3() {
     return (
         <>
-            <div className='w-full h-[600px]'>
-                <div className="card bg-base-100 w-96 shadow-xl">
+            <div className='w-full h-[600px] bg-gray-100 flex flex-col mt-[50px]'>
+                <h1 className='flex justify-center mt-[40px]'>محصولات فروش ویژه</h1>
+                <div className="card bg-base-100 w-96 shadow-xl m-auto mt-[50px]">
                     <figure>
                         <img
-                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                            src="../public/img/Flower-Basket-VIP-250x250.jpg.webp"
                             alt="Shoes" />
                     </figure>
                     <div className="card-body">
                         <h2 className="card-title">
                             Shoes!
-                            <div className="badge badge-secondary">NEW</div>
+                            <div className="">NEW</div>
                         </h2>
                         <p>If a dog chews shoes whose shoes does he choose?</p>
                         <div className="card-actions justify-end">
