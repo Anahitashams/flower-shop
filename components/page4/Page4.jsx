547af1df7b004929a1a8fe8d5fdbcf21e4727364
Anahitashams <anahitashams01@gmail.com>
2025-01-01 00:05:29 -0800

    end

--- components/page4/Page4.jsx
+++ components/page4/Page4.jsx
@@ -4,7 +4,7 @@ function Page4() {
     return (
         <>
             <div className="flex flex-wrap justify-center">
-                <div className="w-[300px] h-[300px] m-2 bg-amber-100 flex flex-col justify-center text-center">
+                <div className="w-[300px] h-[300px] m-2 bg-amber-100 flex flex-col justify-center text-center shadow-neutral-400">
                     <h1>dbvbvd</h1>
                     <p>fvkvokbbpv,vsdfmdfvmovm</p>
                     <p>dvbibv,pd,.dfbffkvwpof,</p>
